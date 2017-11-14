086fcc925a2b1bc8dfba55d8a55e25e7490c11c4
khoa <khoatd19192@gmail.com>
2017-11-14 07:00:30 +0700

    object expressions

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -18,6 +18,6 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
-    return arrayList
+//    Collections.sort(arrayList, object : )
+    return arrayList.sortedDescending()
 }
